53cfad5d69b5fad543550d9dc9afe6c9f2ff9389
qatimokhin <qatimokhin@gmail.com>
2017-11-19 21:13:30 -0800

    Initial commit

---
+++ src/main/java/Items.java
@@ -0,0 +1,56 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by igortimokhin on 11/16/17.
+ */
+public class Items {
+    private static WebDriver driver;
+
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver",
+                "//Users/igortimokhin/Documents/drivers/chromedriver");
+        driver = new ChromeDriver();
+
+        driver.get("file:///Users/igortimokhin/Desktop/FoodItems.html");
+
+//        a)	Display the third and fifth item from the above list. Your output should be:
+//        Applesauce, unsweetened
+//        Juice, Cranberry-apple drink
+
+
+        String item5 = driver.findElement(By.cssSelector("span.title:nth-of-type(5)")).getText();
+        String item9 = driver.findElement(By.cssSelector("span.title:nth-of-type(9)")).getText();
+
+
+        System.out.println(item5 + "\n" + item9 + "\n");
+
+//        List<WebElement> allOptions = driver.findElements(By.cssSelector("span"));
+//            for (WebElement item : allOptions)
+//
+//            System.out.println(item.getText());
+
+//        b)	Fetch each food name and its servings and store them in a Map.
+//          Iterate through all the entries in the Map and print them.
+
+        Map<String, String> map = new HashMap<String, String>();
+        for (int i = 0; i < driver.findElements(By.cssSelector("span.title")).size(); i++) {
+            String titles = driver.findElements(By.cssSelector("span.title")).get(i).getText();
+            String descs = driver.findElements(By.cssSelector("span.description")).get(i).getText();
+            map.put(titles, descs);
+        }
+
+        System.out.println(map);
+
+        driver.quit();
+
+    }
+
+
+}
